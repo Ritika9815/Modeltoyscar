f20669031832929745d15f267786497e3f71227c
Ritika9815 <ritika9815@example.com>
2025-11-15 17:36:51 -0500

    Add header comment with name, date, and description for Lab 3 script

---
+++ script.js
@@ -0,0 +1,10 @@
+/* 
+Name: Ritika Ritika
+Date: 15 November 2025
+Description:
+This script creates a ToyCar constructor following the  style used in class.
+It includes 10–15 properties, a describe() method, and interaction to change color.
+It then outputs the car details to the web page.
+*/
+
+
